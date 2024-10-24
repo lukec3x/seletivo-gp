3de52fcfdbb74f82b873a44d33a77f2d1f1efb74
Lucas Andrade <j.lucasc3x@gmail.com>
2024-10-24 18:53:03 -0300

    Improve job to accept multiple db requests

--- jobs/handleTransactions.ts
+++ jobs/handleTransactions.ts
@@ -1,4 +1,4 @@
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClient } from '@prisma/client';
 
 async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -6,7 +6,7 @@ async function sleep(ms: number) {
 
 (async () => {
   console.log('Starting...');
-  const prisma = new PrismaService();
+  const prisma = new PrismaClient();
 
   while (true) {
     try {
@@ -25,94 +25,99 @@ async function sleep(ms: number) {
       for (const record of records) {
         console.log('Processing record:', record);
 
-        const fromAccount = record.fromAccountId
-          ? await prisma.account.findUnique({
-              where: { id: record.fromAccountId },
-            })
-          : null;
-        const toAccount = record.toAccountId
-          ? await prisma.account.findUnique({
-              where: { id: record.toAccountId },
-            })
-          : null;
+        await prisma.$transaction(async (tx) => {
+          const fromAccount = record.fromAccountId
+            ? await tx.account.findUnique({
+                where: { id: record.fromAccountId },
+              })
+            : null;
+          const toAccount = record.toAccountId
+            ? await tx.account.findUnique({
+                where: { id: record.toAccountId },
+              })
+            : null;
 
-        if (
-          (record.transactionType === 'WITHDRAWAL' ||
-            record.transactionType === 'TRANSFER') &&
-          fromAccount &&
-          fromAccount.accountBalance < record.amount
-        ) {
-          console.log('Insufficient funds for record:', record.id);
-          await prisma.transactionHistory.update({
-            where: { id: record.id },
-            data: { status: 'FAILED' },
-          });
-          continue;
-        }
-
-        try {
-          switch (record.transactionType) {
-            case 'DEPOSIT':
-              if (toAccount) {
-                await prisma.account.update({
-                  where: { id: record.toAccountId },
-                  data: {
-                    accountBalance: {
-                      increment: record.amount,
-                    },
-                  },
-                });
-              }
-              break;
-            case 'WITHDRAWAL':
-              if (fromAccount) {
-                await prisma.account.update({
-                  where: { id: record.fromAccountId },
-                  data: {
-                    accountBalance: {
-                      decrement: record.amount,
-                    },
-                  },
-                });
-              }
-              break;
-            case 'TRANSFER':
-              if (fromAccount && toAccount) {
-                await prisma.account.update({
-                  where: { id: record.fromAccountId },
-                  data: {
-                    accountBalance: {
-                      decrement: record.amount,
-                    },
-                  },
-                });
-                await prisma.account.update({
-                  where: { id: record.toAccountId },
-                  data: {
-                    accountBalance: {
-                      increment: record.amount,
-                    },
-                  },
-                });
-              }
-              break;
-            default:
-              console.log('Invalid transaction type:', record.transactionType);
-              break;
+          if (
+            (record.transactionType === 'WITHDRAWAL' ||
+              record.transactionType === 'TRANSFER') &&
+            fromAccount &&
+            fromAccount.accountBalance < record.amount
+          ) {
+            console.log('Insufficient funds for record:', record.id);
+            await tx.transactionHistory.update({
+              where: { id: record.id },
+              data: { status: 'FAILED' },
+            });
+            return;
           }
 
-          const updatedRecord = await prisma.transactionHistory.update({
-            where: { id: record.id },
-            data: { status: 'COMPLETED' },
-          });
-          console.log('Updated record:', updatedRecord);
-        } catch (error) {
-          console.error('Error processing record:', record.id, error);
-          await prisma.transactionHistory.update({
-            where: { id: record.id },
-            data: { status: 'FAILED' },
-          });
-        }
+          try {
+            switch (record.transactionType) {
+              case 'DEPOSIT':
+                if (toAccount) {
+                  await tx.account.update({
+                    where: { id: record.toAccountId },
+                    data: {
+                      accountBalance: {
+                        increment: record.amount,
+                      },
+                    },
+                  });
+                }
+                break;
+              case 'WITHDRAWAL':
+                if (fromAccount) {
+                  await tx.account.update({
+                    where: { id: record.fromAccountId },
+                    data: {
+                      accountBalance: {
+                        decrement: record.amount,
+                      },
+                    },
+                  });
+                }
+                break;
+              case 'TRANSFER':
+                if (fromAccount && toAccount) {
+                  await tx.account.update({
+                    where: { id: record.fromAccountId },
+                    data: {
+                      accountBalance: {
+                        decrement: record.amount,
+                      },
+                    },
+                  });
+                  await tx.account.update({
+                    where: { id: record.toAccountId },
+                    data: {
+                      accountBalance: {
+                        increment: record.amount,
+                      },
+                    },
+                  });
+                }
+                break;
+              default:
+                console.log(
+                  'Invalid transaction type:',
+                  record.transactionType,
+                );
+                break;
+            }
+
+            const updatedRecord = await tx.transactionHistory.update({
+              where: { id: record.id },
+              data: { status: 'COMPLETED' },
+            });
+            console.log('Updated record:', updatedRecord);
+          } catch (error) {
+            console.error('Error processing record:', record.id, error);
+            await tx.transactionHistory.update({
+              where: { id: record.id },
+              data: { status: 'FAILED' },
+            });
+          }
+        });
       }
 
       await sleep(9_000);
